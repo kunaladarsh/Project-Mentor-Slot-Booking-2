665a5766d0660f1d45e89e7d785cb36649fd1d4f
Adarsh Kunal <78250787+kunaladarsh@users.noreply.github.com>
2023-11-07 10:12:32 +0530

    Update dataBase.sql

--- dataBase.sql
+++ dataBase.sql
@@ -86,15 +86,6 @@ CREATE TABLE group_details (
     mentor_name VARCHAR(255)
 );
 
-INSERT INTO group_details (teacher_id, group_id, group_no, project_title, student_name1, student_name2, student_name3, student_name4, student_name5, sap_id1, sap_id2, sap_id3, sap_id4, sap_id5, specialization1, specialization2, specialization3, specialization4, specialization5, mentor_name)
-VALUES
-(600084634, 101, 1, 'Project A', 'John Smith', 'Alice Johnson', 'Bob Brown', 'Cathy Davis', 'Eva Wilson', 'SAP001', 'SAP002', 'SAP003', 'SAP004', 'SAP005', 'Computer Science', 'Electrical Engineering',
- 'Mechanical Engineering', 'Biomedical Engineering', 'Chemical Engineering', 'Dr. Mentor A');
-
-
-
-
-show tables;
 select * from group_details;
 
 CREATE TABLE group_marks_ac(
@@ -111,13 +102,6 @@ CREATE TABLE group_marks_ac(
 );
 
 
-INSERT INTO group_marks_ac(teacher_id, Group_id,Group_No, Synopsis_Marks, Midsem_Marks, Endsem_Marks, Synopsis_Report, Midsem_Report, Endsem_Report, Mentor_Name)
-VALUES (600084634, 103, 'Group 3', 85, 75, 90, 'https://docs.google.com/document/d/1KfUCpDeSDmMATUKAYA2_MISBvvd22gFX8GB6Wkf8XF0/edit', 'Good', 'Excellent', 'Mr. Smith');
-
-INSERT INTO group_marks_ac(teacher_id, Group_id,Group_No, Synopsis_Marks, Midsem_Marks, Endsem_Marks, Synopsis_Report, Midsem_Report, Endsem_Report, Mentor_Name)
-VALUES (600084634, 103, 'Group 1', 85, 75, 90, 'Pass', 'Good', 'Excellent', 'Mr. Smith');
-
-
 CREATE TABLE group_details (
     teacher_id INT,
     group_id INT,
@@ -129,22 +113,6 @@ CREATE TABLE group_details (
     mentor_name VARCHAR(255)
 );
 
-select * from teacher;
-INSERT INTO group_details (teacher_id, group_id, group_no, project_title, student_name1, student_name2, student_name3, student_name4, student_name5, sap_id1, sap_id2, sap_id3, sap_id4, sap_id5, specialization1, specialization2, specialization3, specialization4, specialization5, mentor_name)
-VALUES
-(600084634, 102, 2, 'mentorslotbooking', 'Shiva', 'Adarsh', 'Aditya Brown', 'Cathy Gaurav', 'Vidya', 'SAP001', 'SAP002', 'SAP003', 'SAP004', 'SAP005', 'Computer Science', 'Electrical Engineering',
- 'Mechanical Engineering', 'Biomedical Engineering', 'Chemical Engineering', 'Dr. Mentor A');
- 
- INSERT INTO group_details (teacher_id, group_id, group_no, project_title, student_name1, student_name2, student_name3, student_name4, student_name5, sap_id1, sap_id2, sap_id3, sap_id4, sap_id5, specialization1, specialization2, specialization3, specialization4, specialization5, mentor_name)
-VALUES
-(600084634, 103, 2, 'mentorslotbooking', 'Vidyananad bhaiya', 'Adarsh', 'Aditya Brown', 'Cathy Gaurav', 'Vidya', 'SAP001', 'SAP002', 'SAP003', 'SAP004', 'SAP005', 'Computer Science', 'Electrical Engineering',
- 'Mechanical Engineering', 'Biomedical Engineering', 'Chemical Engineering', 'Dr. Mentor A');
-
-
 select * from group_details;
 show tables;
 select * from newrequest;
-
-
-
-
